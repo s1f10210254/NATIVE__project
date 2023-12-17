a59baf3d5e6d3998a1c2b13911a643412fc8bc24
s1f10210254 <s1f102102548@iniad.org>
2023-12-17 20:40:13 +0900

    test

--- client/App.tsx
+++ client/App.tsx
@@ -62,6 +62,7 @@ function App(): React.JSX.Element {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
+  const test = 'testです!';
   return (
     <SafeAreaView style={backgroundStyle}>
       <StatusBar
@@ -76,6 +77,7 @@ function App(): React.JSX.Element {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
+          <Text style={{textAlign: 'center'}}>{test}</Text>
           <Section title="Step One">
             Edit <Text style={styles.highlight}>App.tsx</Text> to change this
             screen and then come back to see your edits.
