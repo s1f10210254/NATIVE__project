35a7a4377a80c9b3005953dfd85d258ecb3b7d81
s1f10210254 <s1f102102548@iniad.org>
2023-12-24 01:14:12 +0900

    zod error fix

--- server/service/envValues.ts
+++ server/service/envValues.ts
@@ -9,11 +9,11 @@ const API_BASE_PATH = z
   .startsWith('/')
   .parse(process.env.API_BASE_PATH);
 const CORS_ORIGIN = z.string().url().parse(process.env.CORS_ORIGIN);
-const FIREBASE_AUTH_EMULATOR_HOST = z
-  .string()
-  .optional()
-  .parse(process.env.FIREBASE_AUTH_EMULATOR_HOST);
-const FIREBASE_SERVER_KEY = z.string().parse(process.env.FIREBASE_SERVER_KEY);
+// const FIREBASE_AUTH_EMULATOR_HOST = z
+//   .string()
+//   .optional()
+//   .parse(process.env.FIREBASE_AUTH_EMULATOR_HOST);
+// const FIREBASE_SERVER_KEY = z.string().parse(process.env.FIREBASE_SERVER_KEY);
 const S3_ENDPOINT = z.string().parse(process.env.S3_ENDPOINT ?? '');
 const S3_BUCKET = z.string().parse(process.env.S3_BUCKET ?? '');
 const S3_ACCESS_KEY = z.string().parse(process.env.S3_ACCESS_KEY ?? '');
@@ -23,8 +23,8 @@ const S3_REGION = z.string().parse(process.env.S3_REGION ?? '');
 export {
   API_BASE_PATH,
   CORS_ORIGIN,
-  FIREBASE_AUTH_EMULATOR_HOST,
-  FIREBASE_SERVER_KEY,
+  // FIREBASE_AUTH_EMULATOR_HOST,
+  // FIREBASE_SERVER_KEY,
   PORT,
   S3_ACCESS_KEY,
   S3_BUCKET,
