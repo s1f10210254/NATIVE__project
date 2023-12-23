54ab7111bdd35268a7d2d110aa99a4a39e31e0d8
s1f10210254 <s1f102102548@iniad.org>
2023-12-24 01:34:53 +0900

    s3 error avoidance

--- server/service/s3Client.ts
+++ server/service/s3Client.ts
@@ -1,9 +1,32 @@
-import { S3Client } from '@aws-sdk/client-s3';
-import { S3_ACCESS_KEY, S3_ENDPOINT, S3_REGION, S3_SECRET_KEY } from './envValues';
+import {S3Client} from '@aws-sdk/client-s3';
+import {
+  S3_ACCESS_KEY,
+  S3_ENDPOINT,
+  S3_REGION,
+  S3_SECRET_KEY,
+} from './envValues';
 
-export const s3Client = new S3Client({
-  endpoint: S3_ENDPOINT,
-  region: S3_REGION,
-  forcePathStyle: true,
-  credentials: { accessKeyId: S3_ACCESS_KEY, secretAccessKey: S3_SECRET_KEY },
-});
+// export const s3Client = new S3Client({
+//   endpoint: S3_ENDPOINT,
+//   region: S3_REGION,
+//   forcePathStyle: true,
+//   credentials: { accessKeyId: S3_ACCESS_KEY, secretAccessKey: S3_SECRET_KEY },
+// });
+
+let s3Client;
+
+if (S3_REGION && S3_ACCESS_KEY && S3_SECRET_KEY) {
+  s3Client = new S3Client({
+    endpoint: S3_ENDPOINT,
+    region: S3_REGION,
+    forcePathStyle: true,
+    credentials: {accessKeyId: S3_ACCESS_KEY, secretAccessKey: S3_SECRET_KEY},
+  });
+} else {
+  // S3はローカル環境でのみ使用される
+  console.log(
+    'S3 configuration is missing. S3 Client will not be initialized.',
+  );
+}
+
+export {s3Client};
