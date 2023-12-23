3c178eae2a5b4c2f80658bc31ec2501b7ae75a65
s1f10210254 <s1f102102548@iniad.org>
2023-12-24 01:08:38 +0900

    status add

--- client/src/screens/PrismaScreen.tsx
+++ client/src/screens/PrismaScreen.tsx
@@ -18,15 +18,32 @@ const PrismaScreen = ({navigation}: Props) => {
     id: string;
   }[];
   const [text, setText] = useState<user | null>(null);
+  const [health, setHealth] = useState<
+    Record<'server' | 'db', 'ok' | 'ng'> | undefined
+  >();
+  const healthTest = async () => {
+    const data = await apiClient.health.$get();
+    setHealth(data);
+  };
   const getUser = async () => {
     const id = await apiClient.prisma.$get().catch(null);
     setText(id);
   };
+
   useEffect(() => {
+    healthTest();
     getUser();
   }, []);
   return (
     <View>
+      <Text>health ok?</Text>
+      {health && (
+        <View>
+          <Text>Server Status: {health.server}</Text>
+          <Text>Database Status: {health.db}</Text>
+        </View>
+      )}
+
       {text && text.map((user, index) => <Text key={index}>{user.id}</Text>)}
     </View>
   );
