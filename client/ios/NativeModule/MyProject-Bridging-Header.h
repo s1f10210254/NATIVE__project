5b11733256924a5e3e3f3f73a403890580ce911b
s1f10210254 <s1f102102548@iniad.org>
2023-12-29 04:00:12 +0900

    request

--- client/ios/NativeModule/MyProject-Bridging-Header.h
+++ client/ios/NativeModule/MyProject-Bridging-Header.h
@@ -1,16 +1,3 @@
 #import "AppDelegate.h"
 #import <React/RCTBridgeModule.h>
 #import <React/RCTViewManager.h>
-
-
-#import <Foundation/Foundation.h>
-#import <React/RCTUIManager.h>
-#import <React/RCTEventEmitter.h>
-
-#import <React/RCTBridge.h>
-#import <React/RCTRootView.h>
-
-#ifndef MyProject-_Bridging_Header_h
-#define MyProject-_Bridging_Header_h
-
-#endif
